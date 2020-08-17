bc0a4d870426437802152d4c0bb03d1ba7fc3c51
Lucas Sorenson <lsorenson20@gmail.com>
2020-08-17 09:03:50 -0500

    add the unreal project

---
+++ prototype/DuneVR/Source/DuneVR/DuneVRGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DuneVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUNEVR_API ADuneVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
