f9209447fc841a23ae1eeac8c5b2a04a98451800
Lucas Sorenson <lsorenson20@gmail.com>
2020-08-29 18:05:40 -0500

    initial prototype

---
+++ prototype/DuneVR/ImportAsset.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+UE4_Version='4.24'
+CurrentDirectory="`dirname \"$0\"`"
+FilePath=$1
+AssetDump=/Game/$2/$3
+
+/Users/Shared/Epic\ Games/UE_$UE4_Version/Engine/Binaries/Mac/UE4Editor-Cmd \
+-project=DuneVR.uproject \
+-run=ImportAssets \
+-source=$FilePath \
+-dest=$AssetDump \
+-importsettings="" \
+-AllowCommandletRendering
+
+#This is where the files are dumped
+#open /Users/lucassorenson/Library/Application\ Support/Epic/UnrealEngine/$UE4_Version"/Content/"$2/$3
+
+mkdir -p $CurrentDirectory/Content/$2
+
+cp -a /Users/lucassorenson/Library/Application\ Support/Epic/UnrealEngine/$UE4_Version"/Content/"$2/$3 $CurrentDirectory/Content/$2
