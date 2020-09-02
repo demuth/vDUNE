f3a84e7f7f59d60dea8f438542f5d20c43643e70
Lucas Sorenson <lsorenson20@gmail.com>
2020-09-01 20:30:02 -0500

    Create a basic TCO class to interact with the avatar

---
+++ prototype/DuneVR/Source/DuneVR/Private/Detector/CryostatTCO.cpp
@@ -0,0 +1,40 @@
+#include "CryostatTCO.h"
+#include "Engine/StaticMeshActor.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+ACryostatTCO::ACryostatTCO()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+    USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>( TEXT("RootComponent") );
+    RootComponent = SphereComponent;
+    SphereComponent->InitSphereRadius( 40.0f );
+}
+
+void ACryostatTCO::PreInitializeComponents()
+{
+    Super::PreInitializeComponents();
+
+    for( auto mesh : mesh_list_ )
+    {
+        auto actor = GetWorld()->SpawnActor<AStaticMeshActor>(FVector(), FRotator());
+        actor->GetStaticMeshComponent()->SetStaticMesh(mesh);
+        actor->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+    }
+}
+
+// Called when the game starts or when spawned
+void ACryostatTCO::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ACryostatTCO::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ prototype/DuneVR/Source/DuneVR/Private/Detector/CryostatTCO.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CryostatTCO.generated.h"
+
+
+UCLASS()
+class ACryostatTCO : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACryostatTCO();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void PreInitializeComponents() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Mesh)
+	TArray<UStaticMesh*> mesh_list_;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
