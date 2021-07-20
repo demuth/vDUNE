a22897079295041a8e4b69f2b2255efcc0025631
lmsorenson <lsorenson20@gmail.com>
2021-07-20 15:42:40 -0500

    start scripting asset downloads

--- scripts/acquire_assets.sh
+++ scripts/acquire_assets.sh
@@ -1,3 +1,25 @@
-#!\bin\bash
+#!/bin/bash
 
-wget -r -np -R "index.html*" https://edms.cern.ch/file/2173180/7/
+cern="https://edms.cern.ch/file/"
+path=/Users/lucassorenson/Code/dune/Subversion/trunk/assets/files/
+asset=2173180/6/
+
+files=(Cavern_S_-_Chamber_ES_-_Warm_Cryostat_SP_-_Primary_Membrane_-_FULL \
+Cavern_S_-_Chamber_ES_-_Warm_Cryostat_SP_-_Primary_Membrane_-_Bottom \
+Cavern_S_-_Chamber_ES_-_Warm_Cryostat_SP_-_Primary_Membrane_-_Top \
+Cavern_S_-_Chamber_ES_-_Warm_Cryostat_SP_-_Primary_Membrane_-_North \
+Cavern_S_-_Chamber_ES_-_Warm_Cryostat_SP_-_Primary_Membrane_-_South \
+Cavern_S_-_Chamber_ES_-_Warm_Cryostat_SP_-_Primary_Membrane_-_East \
+Cavern_S_-_Chamber_ES_-_Warm_Cryostat_SP_-_Primary_Membrane_-_West)
+
+extensions=(.png .nwd)
+
+mkdir -p ${path}${asset}
+
+for filename in ${files[@]}; do
+  for extension in ${extensions[@]}; do
+    wget \
+    -O ${path}${asset}${filename}${extension} \
+    ${cern}${asset}${filename}${extension}
+  done
+done
