d6ea52cc8f423e922d28a52a92e7d35d4a636e21
lmsorenson <lsorenson20@gmail.com>
2021-07-20 18:16:41 -0500

    start scripting asset downloads

--- scripts/acquire_assets.command
+++ scripts/acquire_assets.command
@@ -1,3 +1,7 @@
-#!\bin\bash
+#!/bin/bash
 
-./acquire_assets.sh
+$(dirname "$0")/acquire_assets.sh 2173180 6
+$(dirname "$0")/acquire_assets.sh 2173180 7
+
+$(dirname "$0")/acquire_assets.sh 2067825 6
+$(dirname "$0")/acquire_assets.sh 2067825 7
--- scripts/acquire_assets.sh
+++ scripts/acquire_assets.sh
@@ -2,17 +2,28 @@
 
 cern="https://edms.cern.ch/file/"
 path=$(dirname $(dirname "$0"))/assets/files/
-asset=2173180/6/
+asset=$1/$2/
 
+echo "Reading config: $(dirname "$0")/Config/$1"
+
+# Read a config file in.
 files=()
 while IFS= read -r line; do
    files+=("$line")
-done <$(dirname "$0")/Config/2173180.7
+done <$(dirname "$0")/Config/$1
 
+for filename in ${files[@]}; do
+  echo "Read: ${filename}"
+done
+
+# Create an array of desired formats.
 extensions=(.png .nwd)
 
+# Make sure the asset directory exists before writing to it.
 mkdir -p ${path}${asset}
 
+# Walk through each combination of file stem and extension.
+# Retrieving those files from CERN
 for filename in ${files[@]}; do
   for extension in ${extensions[@]}; do
     wget \
